e32b8370d93ee8d445403891c34b20aad6d32cf6
a1exprod <alexander.schnabl@outlook.com>
2024-07-02 10:18:16 +0200

    Update in Landingpage
    ab hier wird auf github pages veröffentlicht

---
+++ js/anchor-navigation.js
@@ -0,0 +1,7 @@
+function scrollTo_indexServices() {
+    document.getElementById('anchor-index-services').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollTo_projects() {
+    window.location.href = '../html/projects.html';
+}
--- php/contact.php
+++ php/contact.php
@@ -1,15 +1,11 @@
 <?php
+    $emailadressReceive = "alexander.schnabl5@gmail.com"; // Define here on which email address the messages from the web should go.
 
-    $emailadressReceive = "alexander.schnabl5@gmail.com" //Define here on which email adress the messages from the web should go.
-
-
-    
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
         $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-        $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $message = filter_var($_POST['message'], FILTER_SANITIZE_SPECIAL_CHARS);
 
         if (empty($name) || empty($email) || empty($message)) {
             echo "Bitte füllen Sie alle Pflichtfelder aus.";
@@ -32,9 +28,15 @@
         if (mail($to, $subject, $fullMessage, $headers)) {
             echo "Vielen Dank für Ihre Nachricht. Wir werden uns so schnell wie möglich bei Ihnen melden.";
         } else {
-            echo "Entschuldigung, beim Senden Ihrer Nachricht ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.";
+            echo "<p>Entschuldigung, beim Senden Ihrer Nachricht ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.</p>";
         }
     } else {
         echo "Ungültige Anforderung.";
     }
 ?>
+
+<!--
+| Warning: mail(): SMTP server response: 553 We do not relay non-local mail, sorry. in E:\unitas-homepage\php\contact.php on line 28
+| -> Mailserver muss bei der fertigen webseite (am server) installiert werden. ->SMTP-Server .. PHPMailer(composer) oder/und Gmail
+-->
+
